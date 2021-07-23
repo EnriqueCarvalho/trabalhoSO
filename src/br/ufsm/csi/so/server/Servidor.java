bf80fdfc7e52010231a3d0e614661d520a05feba
enriquecarvalho <enriquecarvalho60@gmail.com>
2021-07-23 18:49:39 -0300

    Agr vai

--- src/br/ufsm/csi/so/server/Servidor.java
+++ src/br/ufsm/csi/so/server/Servidor.java
@@ -17,17 +17,11 @@ import java.util.ArrayList;
 
 
 public class Servidor {
-
-
-
     public static void main(String[] args) throws IOException {
         ArrayList <Reserva> reservas = new ArrayList();
 
+
         ServerSocket serverSocket = new ServerSocket(80);
-        byte[] log = new byte[2048];
-        Semaphore vazio = new Semaphore(TamBuf);
-        Semaphore cheio = new Semaphore(0);
-        Semaphore mutex = new Semaphore(1);
 
             while (true) {
                 try {
@@ -36,14 +30,14 @@ public class Servidor {
                     byte[] buffer = new byte[2048];
                     int len = in.read(buffer);
                     String requisicao = new String(buffer, 0, len);
-                    Object requisicao2 = new String(buffer, 0, len);
-                    System.out.println(buffer);
-                    System.out.println(requisicao2);
+
+                  /*  System.out.println(buffer);*/
+
                     
                     String[] lines = requisicao.split("\n");
-                    String[] linha0 = lines[0].split(" ");
+                     String[] linha0 = lines[0].split(" ");
 
-                    System.out.println("Comando: " + linha0[0] + " Recurso: " + linha0[1]);
+                  /*  System.out.println("Comando: " + linha0[0] + " Recurso: " + linha0[1]);*/
                     OutputStream out = socket.getOutputStream();
                     File f = new File("resources" + File.separator + linha0[1]);
 
@@ -53,24 +47,41 @@ public class Servidor {
                     }else if (linha0[1].startsWith("/solicitar")){
                      f = new File("resources" + File.separator + "solicitar.html");
                         //produtorConsumidor
-                    String[] aux = linha0[1].split("\\?");
-                     String id = aux[1].substring(4,6);
-                     System.out.println(id);
 
 
-                    }else if (linha0[1].equals("/finalizar")){
-                        Reserva res= new Reserva(1,true,"123","02/02/02");
-                        reservas.add(res);
+
+                    }else if (linha0[1].startsWith("/finalizar")){
+                        String nome = Service.getNomeStr(linha0[1]);
+                        Integer numAssento= Service.getAssentoInt(linha0[1]);
+
+                            if (!reservas.isEmpty()) {
+                                if(Reserva.verificarLugares(numAssento,reservas)){
+                                    System.out.println("[RESERVADO]");
+                                }else{
+                                    System.out.println("[NOVA RESERVA E JA EXISTIA]");
+                                    Reserva novaReserva = new Reserva(numAssento,true,nome,"01/02/02");
+                                    reservas.add( novaReserva);
+                                }
+                            }else{
+                                System.out.println("NOVA RESERVA");
+                                Reserva novaReserva = new Reserva(numAssento,true,nome,"01/02/02");
+                                reservas.add( novaReserva);
+                            }
+
+                        f = new File("resources" + File.separator + "index.html");
+
+
+
 
                     }else if (linha0[1].equals("/js/index.js")){
-                        String js = Service.montaJS();
+                        String js = Service.montaJS(reservas);
                         out.write(js.getBytes());
 
                     }
                     if (f.exists() && !linha0[1].equals("/js/index.js") ) {
                         FileInputStream fin = new FileInputStream(f);
                         String mimeType = Files.probeContentType(f.toPath());
-                        System.out.println("servindo o arquivo " + f + "(" + mimeType + ")");
+
                         out.write(("HTTP/1.1 200 OK\n" +
                                 "Content-Type: " + mimeType + ";charset=UTF-8\n\n").getBytes());
                         len = fin.read(buffer);
