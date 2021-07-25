2cf36908961dadf07125ea1b69a75b938574c27e
angelormar <angelomarramon@gmail.com>
2021-07-25 18:42:01 -0300

    servidor recuperado

---
+++ src/br/ufsm/csi/so/server/GerenciaLog.java
@@ -0,0 +1,106 @@
+package br.ufsm.csi.so.server;
+
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.net.InetAddress;
+import java.net.Socket;
+import java.util.concurrent.Semaphore;
+
+public class GerenciaLog {
+
+    private static int tamBuf = 100;
+    private static byte[] logBuffer = new byte[tamBuf];
+    private Socket Socket;
+    private InetAddress ipReservado;
+    Semaphore vazio = new Semaphore(tamBuf);
+    Semaphore cheio = new Semaphore(0);
+    Semaphore mutex = new Semaphore(1);
+    private static int iStatic;
+    private static File file = new File("log.txt");
+    private String requisicao;
+    private String dataHora;
+
+    public GerenciaLog(Socket socket, String requisicao, String dataHora) {
+        this.dataHora = dataHora;
+        this.requisicao = requisicao;
+        Socket = socket;
+
+        try {
+            if (file.createNewFile()) {
+                System.out.println("Arquivo criado: " + file.getName());
+            }
+        } catch (IOException e1) {
+            e1.printStackTrace();
+        }
+
+        new Thread(new GerenciaLog.ProduzLog()).start();
+        new Thread(new GerenciaLog.ArmazenaLog()).start();
+
+    }
+
+    private class ProduzLog implements Runnable {
+
+        @Override
+        public void run() {
+           
+          ipReservado = Socket.getInetAddress();
+            String ipString = ipReservado.toString();
+            String[] endereco = requisicao.split("Host");
+            ipString = "\nIp:"  + ipString +  "Endereco: " + endereco[0] + "Data e hora: "+  dataHora +  " \n";
+  
+            byte[] ipBype = ipString.getBytes();
+            int tamBufAtual = logBuffer.length - iStatic;
+
+            try {
+                vazio.acquire(ipBype.length);
+                mutex.acquire();
+            } catch (InterruptedException e) {
+            }
+            System.out.println("gravou");
+            for (int i = 0; i < ipBype.length; i++) {
+                logBuffer[iStatic] = ipBype[i];
+                iStatic++;
+            }
+            mutex.release();
+            cheio.release();
+        
+        }
+
+    }
+
+    private class ArmazenaLog implements Runnable {
+
+        @Override
+        public void run() {
+
+    try {
+        cheio.acquire();
+        mutex.acquire();
+    } catch (InterruptedException e) {
+        // TODO: handle exception
+    }
+    
+    String logString = new String(logBuffer, 0, iStatic);
+    int tamanhoUsado = iStatic;
+    iStatic = 0;
+    logBuffer = new byte[tamBuf];
+    
+    System.out.println("consumiu");
+    mutex.release();
+    vazio.release(tamanhoUsado);
+    
+    try {
+        FileWriter myWriter = new FileWriter("log.txt", true);
+        myWriter.write(logString);
+        myWriter.close();
+    } catch (IOException e) {
+        System.out.println("Erro ao gravar os dados");
+        e.printStackTrace();
+    }
+    
+
+        }
+
+    }
+}
--- src/br/ufsm/csi/so/server/Service.java
+++ src/br/ufsm/csi/so/server/Service.java
@@ -5,8 +5,20 @@ public class Service {
     public static String montaJS(){
         String header;
         String body;
-        header = "\"HTTP/1.1 200 OK\\n\" +\n" +
-                " \"Content-Type: text/javascript;charset=UTF-8\\n\\n\"";
+        header = "HTTP/1.1 200 OK\n"+
+                " Content-Type: text/javascript;charset=UTF-8\n\n";
+
+       body = "";
+        for (Reserva res : reservas) {
+            if(res.isReservado()) {
+                body=body+" document.getElementById('" + res.getNumAssento() + "').classList.add('ocupado')\n";
+                body = body+ " $('#teste').append('<tr> <th>"+res.getNumAssento()+"</th><td>"+res.getNome()+"</td><td>"+res.getData()+"</td></tr>')\n";
+            }
+        }
+
+
+        return header.concat(body);
+    }
 
         body = "";
 
